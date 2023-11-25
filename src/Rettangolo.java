7560158e844cdc948c95d7e821db9ee9f92220e0
elianBertola <150374006+elianBertola@users.noreply.github.com>
2023-11-25 18:43:44 +0100

    Add files via upload

---
+++ src/Rettangolo.java
@@ -0,0 +1,31 @@
+public class Rettangolo extends Forma{
+    private int base;
+    private int altezza;
+
+    public Rettangolo(int base, int altezza) {
+        this.base = base;
+        this.altezza = altezza;
+    }
+
+    public int getBase() {
+        return base;
+    }
+
+    public void setBase(int base) {
+        this.base = base;
+    }
+
+    public int getAltezza() {
+        return altezza;
+    }
+
+    public void setAltezza(int altezza) {
+        this.altezza = altezza;
+    }
+
+    @Override
+    public void calcolaArea() {
+        int area = base * altezza;
+        System.out.println("L'area del rettangolo e: " + area + "cm");
+    }
+}
